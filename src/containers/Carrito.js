65530a1a15fc49092d490593829f6d6a8be80c8b
Jimena Diewiatajew <jimediew@gmail.com>
2022-02-11 16:07:55 -0300

    create containers

---
+++ src/containers/Carrito.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Carrito (){
+
+    return(
+
+        <div>
+            <h1>Carrito</h1>
+        </div>
+
+    )
+
+}
---
+++ src/containers/Producto.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Producto (){
+
+    return(
+
+        <div>
+            <h1>Producto</h1>
+        </div>
+
+    )
+
+}
