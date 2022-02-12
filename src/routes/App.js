5ffb3a63c24b61a7ecd7ba604d16a477b92721b2
Jimena Diewiatajew <jimediew@gmail.com>
2022-02-12 14:34:01 -0300

    delete bootstrap

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,16 +1,23 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../containers/Home";
+import Carrito from "../containers/Carrito";
+import Producto from "../containers/Producto";
+import Layout from "../components/Layout";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-  crossorigin="anonymous"
-/>
-     <h1>DevStore</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/carrito" element={<Carrito />}></Route>
+            <Route exact path="/producto" element={<Producto />}></Route>
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </>
   );
 }
 
