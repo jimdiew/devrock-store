4dfe903b530aa5b7bfe8caac2cdeafa7e3b88d08
Jimena Diewiatajew <jimediew@gmail.com>
2022-02-12 14:34:09 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './routes/App'; 
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./routes/App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
