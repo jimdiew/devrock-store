6d880efe79f3d19d70f24f4c84af549cbc87ce77
Jimena Diewiatajew <jimediew@gmail.com>
2022-02-12 21:16:37 -0300

    iterate items

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "../assets/css/Home.css";
 import Item from "../components/Item";
+import Contexto from "../context/Contexto";
 
 export default function Home() {
+  useEffect(() => {
+    listameProductos();
+  }, []);
+  const { listameProductos, productos } = useContext(Contexto);
   return (
     <>
       <div className="container">
         <div className="wraper">
           <div className="home">
-            <Item></Item>
-            <Item></Item>
-            <Item></Item>
-            <Item></Item>
-            <Item></Item>
-            <Item></Item>
+            {productos.map((item) => (
+              <Item {...item}></Item>
+            ))}
           </div>
         </div>
       </div>
