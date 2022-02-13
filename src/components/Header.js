6ba67c4922947318df145d03dedfce71e7e121af
Jimena Diewiatajew <jimediew@gmail.com>
2022-02-13 01:09:36 -0300

    change a to Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
 import iconoCarrito from "../assets/statics/carrito.png";
 import iconoVolver from "../assets/statics/volver.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <>
-      <a href="carrito.html">
+      <Link to="/carrito">
         <img src={iconoCarrito} alt="" className="carritou" />
-      </a>
-      <a href="">
+      </Link>
+      <Link to="/">
         <img src={iconoVolver} alt="" className="volver" />
-      </a>
+      </Link>
       <h1 className="titulo">
         Dev <br />
         RockStore
